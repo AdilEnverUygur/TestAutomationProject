78c5edee21a21f3d7b341e9e874e5639df828cbb
Adil Enver <adiljan.anwar@gmail.com>
2022-03-26 13:04:39 +0300

    Catalog Dashboard Page

---
+++ src/main/java/com/ownProject/backEnd/catalogModule/CatalogDashboardPage.java
@@ -0,0 +1,42 @@
+package com.ownProject.backEnd.catalogModule;
+
+import com.ownProject.testUtility.TestUtility;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class CatalogDashboardPage {
+    WebDriver driver;
+    TestUtility testUtility;
+    Actions actions;
+
+    @FindBy(id = "nav")
+    WebElement catalogDropdownList;
+    @FindBy(xpath = "//span[contains(text(),'Manage Products')]")
+    WebElement manageProductsLink;
+    @FindBy(css = ".link-logout")
+    WebElement logoutButton;
+
+    public CatalogDashboardPage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+        testUtility = new TestUtility(driver);
+    }
+
+    public void moveMouseOverCatalogLink(){
+        testUtility.waitForElementPresent(catalogDropdownList);
+        actions = new Actions(driver);
+        actions.moveToElement(catalogDropdownList).build().perform();
+    }
+
+    public boolean verifyLogin(){
+        testUtility.waitForElementPresent(logoutButton);
+        System.out.println("Login Successful");
+        return logoutButton.isDisplayed();
+    }
+
+
+}
